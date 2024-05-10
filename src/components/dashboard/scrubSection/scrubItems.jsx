b3037747b42f30412b5a8a062da37fa428e69616
Muhammad Aitisam Yaseen <145204832+MaitisamY@users.noreply.github.com>
2024-05-10 15:26:04 -0700

    table options added

--- src/components/dashboard/scrubSection/scrubItems.jsx
+++ src/components/dashboard/scrubSection/scrubItems.jsx
@@ -1,18 +1,53 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { STATES } from '../../../utils/usaStates.js';
+import { MdCheckBoxOutlineBlank, MdCheckBox, MdOutlineFileDownload } from 'react-icons/md';
+import { FaTrashAlt, FaCogs, FaFileAlt } from 'react-icons/fa';
+import { saveAs } from 'file-saver';
+import axios from 'axios';
 
 function ScrubItems({ scrubItems }) {
 
     const [statesData, setStatesData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [openDropdownIndex, setOpenDropdownIndex] = useState(null);
+    const dropdownRefs = useRef([]);
 
     useEffect(() => {
+        dropdownRefs.current = 
+        Array(scrubItems.length).fill(0).map((_, i) => dropdownRefs.current[i] || React.createRef());
+        
         STATES ? setStatesData(STATES) : setStatesData([]);
-        // Simulate loading delay
         setTimeout(() => {
             setIsLoading(false);
-        }, 3000); // 3000 milliseconds delay
+        }, 3000);
     }, []);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.target.closest('.dropdown-button')) {
+                setOpenDropdownIndex(null);
+            }
+        };
+        
+        window.addEventListener('click', handleClickOutside);
+
+        return () => {
+            window.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    const handleSelection = (id) => {
+        if (selectedItems.includes(id)) {
+            setSelectedItems(selectedItems.filter(item => item !== id))
+        } else {
+            setSelectedItems([...selectedItems, id])
+        }
+    }
+
+    const handleIsDropped = (index) => {
+        setOpenDropdownIndex(openDropdownIndex === index ? null : index);
+    }
 
     const getStateNames = (stateIds) => {
         const stateNames = [];
@@ -25,6 +60,54 @@ function ScrubItems({ scrubItems }) {
         });
         return stateNames.join(', '); // Join state names with comma and space
     };
+
+    const downloadMatchingFile =  (fileName) => {
+        fetch(`http://localhost:3000/download/matching-file/${encodeURIComponent(fileName)}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                saveAs(blob, fileName);
+            })
+            .catch(error => {
+                console.error('Error downloading matching file:', error);
+            });
+    }
+    
+    const downloadNonMatchingFile = (fileName) => {
+        fetch(`http://localhost:3000/download/non-matching-file/${encodeURIComponent(fileName)}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                saveAs(blob, fileName);
+            })
+            .catch(error => {
+                console.error('Error downloading non-matching file:', error);
+            });
+    }
+
+    const downloadUploadedFile = (fileName) => {
+        fetch(`http://localhost:3000/download/uploaded-file/${encodeURIComponent(fileName)}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                saveAs(blob, fileName);
+            })
+            .catch(error => {
+                console.error('Error downloading file:', error);
+            });
+    }
 
     return (
         <>
@@ -40,39 +123,163 @@ function ScrubItems({ scrubItems }) {
                         borderBottom: '4px solid #104cba',
                     }}
                 ></div>
-            ) : (
+            ) : ( 
                 <>
-                    <h3>Scrub History</h3>
+                    <h3 style={{ width: '100%', textAlign: 'center' }}>Scrub History</h3>
                     {scrubItems.length === 0 ? (
                         <div>
-                            <p>Your Scrub History Will Appear Below</p>
-                            <p>You haven't scrubbed any files yet.</p>
+                            <p className="text-red">You haven't scrubbed any files yet.</p>
                         </div>
                     ) : (
                         <table>
                             <thead>
                                 <tr>
+                                    <th>
+                                        <div style={{ cursor: 'pointer' }}>
+                                            {
+                                                selectedItems.length > 0 ? (
+                                                    <MdCheckBox 
+                                                        size={20} 
+                                                        style={{ color: '#fff' }}
+                                                        onClick={() => setSelectedItems([])} 
+                                                    />
+                                                ) : (
+                                                    <MdCheckBoxOutlineBlank 
+                                                        size={20} 
+                                                        onClick={() => setSelectedItems(scrubItems.map(item => item.id))} 
+                                                    />
+                                                )
+                                            }
+                                        </div>
+                                    </th>
+                                    <th>#</th>
                                     <th>Date</th>
                                     <th>Uploaded File</th>
-                                    <th>States</th>
-                                    <th>Scrubbed Options</th>
+                                    <th>Scrub States</th>
+                                    <th>Scrub Options</th>
                                     <th>Total Numbers</th>
                                     <th>Clean Numbers</th>
                                     <th>Bad Numbers</th>
                                     <th>Cost</th>
+                                    <th>
+                                        {
+                                            selectedItems && selectedItems.length > 1 && (
+                                                <div className="delete-selection">
+                                                    <FaTrashAlt size={15} /> <span>Delete ({selectedItems.length})</span>
+                                                </div>
+                                            )
+                                        }
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {scrubItems.map((item, index) => (
                                     <tr key={index}>
+                                        <td>
+                                            <div style={{ cursor: 'pointer'}}>
+                                                {
+                                                    selectedItems.includes(item.id) ? (
+                                                        <MdCheckBox 
+                                                            size={20} 
+                                                            style={{ cursor: 'pointer', color: '#108d10' }}
+                                                            onClick={() => handleSelection(item.id)} 
+                                                        />
+                                                    ) : (
+                                                        <MdCheckBoxOutlineBlank 
+                                                            size={20} 
+                                                            onClick={() => handleSelection(item.id)} 
+                                                        />
+                                                    )
+                                                }
+                                            </div>
+                                        </td>
+                                        <td>{scrubItems.indexOf(item) + 1}</td>
                                         <td>{item.date}</td>
-                                        <td>{item.uploaded_file.slice(0, 7) + '.....' + item.uploaded_file.slice(-7)}</td>
-                                        <td>{getStateNames(item.scrubbed_against_states)}</td>
-                                        <td>{item.scrubbed_against_options}</td>
+                                        <td>
+                                            <a 
+                                                className="file-link" 
+                                                onClick={() => downloadUploadedFile(item.uploaded_file)}
+                                            >
+                                                {
+                                                    item.uploaded_file.slice(0, 12) 
+                                                    + ' ..... ' + 
+                                                    item.uploaded_file.slice(-8)
+                                                }
+                                            </a>
+                                        </td>
+                                        <td>
+                                            {
+                                                getStateNames(item.scrubbed_against_states) ?
+                                                getStateNames(item.scrubbed_against_states) :
+                                                '--'
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                item.scrubbed_against_options ?
+                                                item.scrubbed_against_options :
+                                                '--'
+                                            }
+                                        </td>
                                         <td>{item.total_numbers}</td>
                                         <td>{item.clean_numbers}</td>
                                         <td>{item.bad_numbers}</td>
                                         <td>{item.cost}</td>
+                                        <td>
+                                            <button 
+                                                className="dropdown-button"
+                                                style={{ color: '#ffffff' }} 
+                                                onClick={() => handleIsDropped(index)}
+                                            >
+                                                <FaCogs size={15} /> Options
+                                                {openDropdownIndex === index && (
+                                                    <div className="options-dropdown" ref={dropdownRefs.current[index]}>
+                                                        <ul>
+                                                            <li>
+                                                                <a 
+                                                                    
+                                                                    download 
+                                                                    className="green"
+                                                                >
+                                                                    <FaFileAlt size={15} />
+                                                                    <span>View</span>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a 
+                                                                    onClick={() => downloadMatchingFile(item.matching_file)} 
+                                                                    download 
+                                                                    className="green"
+                                                                >
+                                                                    <MdOutlineFileDownload size={20} />
+                                                                    <span>Clean Numbers</span>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a  
+                                                                    onClick={() => downloadNonMatchingFile(item.non_matching_file)}
+                                                                    download 
+                                                                    className="green"
+                                                                >
+                                                                    <MdOutlineFileDownload size={20} />
+                                                                    <span>Bad Numbers</span>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a 
+                                                                     
+                                                                    download 
+                                                                    className="red"
+                                                                >
+                                                                    <FaTrashAlt size={15} />
+                                                                    <span>Delete</span>
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                )}
+                                            </button>
+                                        </td>
                                     </tr>
                                 ))}
                             </tbody>
